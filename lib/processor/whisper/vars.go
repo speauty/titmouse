497b82971bbef4b869bfcacb4354aba2d573016b
speauty <speauty@163.com>
2023-04-11 23:09:11 +0800

    update: 增加一些常见语种;

--- lib/processor/whisper/vars.go
+++ lib/processor/whisper/vars.go
@@ -1,8 +1,11 @@
 package whisper
 
 var (
-	LanguagesSupported = []string{"英文", "中文"}
-	languagesMap       = map[string]string{
-		"英文": "en", "中文": "zh",
+	LanguagesSupported = []string{
+		"英文", "中文", "德语", "西班牙语", "俄罗斯语", "韩语", "法语", "日语", "葡萄牙语",
+	}
+	languagesMap = map[string]string{
+		"英文": "en", "中文": "zh", "德语": "de", "西班牙语": "es", "俄罗斯语": "ru",
+		"韩语": "ko", "法语": "fr", "日语": "ja", "葡萄牙语": "pt",
 	}
 )
